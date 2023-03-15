1ca5ad647c570b19f01653e82e9363bb65108b99
Dante Issaias <dante@issaias.com>
2023-03-15 20:44:34 +0000

    Fix

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import { createServer } from './server';
 
 createServer({}).then((server) => {
-  server.listen(3000, () =>
-    console.log('> Dash started on http://localhost:3000')
+  server.listen(5555, () =>
+    console.log('> Dash started on http://localhost:5555')
   );
 });
