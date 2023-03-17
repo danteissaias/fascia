226c2b8cc42211c2ff0d953000b4b032e286c70d
Dante Issaias <dante@issaias.com>
2023-03-17 12:42:19 +0000

    Actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,13 @@
-import { DataTable, Stack, Text, toast } from '@danteissaias/ds';
+import {
+  Button,
+  DataTable,
+  IconButton,
+  Stack,
+  Text,
+  toast,
+} from '@danteissaias/ds';
 import { useEffect, useState } from 'react';
-import { Trash } from 'react-feather';
+import { RotateCw, Trash } from 'react-feather';
 import type { Config, DocumentAction, Schema } from '../config';
 
 interface AppProps {
@@ -9,7 +16,7 @@ interface AppProps {
 
 export default function App({ config }: AppProps) {
   return (
-    <Stack m="24">
+    <Stack align="center">
       {Object.entries(config.schemas).map(([key, schema], i) => (
         <ModelView key={key} modelName={key} schema={schema} />
       ))}
@@ -26,6 +33,15 @@ const deleteAction: DocumentAction<any> = ({ document, removeDocument }) => ({
     toast.success(`Deleted record ${document.id}`);
   },
 });
+
+const headerActions = (rows: any[]) => [
+  {
+    label:
+      rows.length === 1 ? 'Delete 1 record' : `Delete ${rows.length} records`,
+    danger: true,
+    onHandle: () => console.log('Delete', rows),
+  },
+];
 
 const rpc = async (modelName: string, operation: string, args: any) => {
   return await fetch('/rpc', {
@@ -59,8 +75,8 @@ function ModelView<T>({ modelName, schema }: ModelViewProps<T>) {
   };
 
   return data ? (
-    <Stack m="24" gap="12" style={{ maxWidth: 800 }}>
-      <Text size="20" mb="16">
+    <Stack m="24" gap="12" style={{ maxWidth: 1000, width: '100%' }}>
+      <Text size="20" mb="12">
         {modelName}
       </Text>
 
@@ -68,6 +84,7 @@ function ModelView<T>({ modelName, schema }: ModelViewProps<T>) {
         selectable
         columns={schema.columns}
         data={data}
+        headerActions={headerActions}
         rowActions={(document) =>
           schema.actions
             .concat([deleteAction])
@@ -75,15 +92,18 @@ function ModelView<T>({ modelName, schema }: ModelViewProps<T>) {
               action({ document, removeDocument: removeDocument(document) })
             )
         }
-      />
+      >
+        <IconButton size="1">
+          <RotateCw />
+        </IconButton>
+        <Button size="1">Add record</Button>
+      </DataTable>
     </Stack>
   ) : (
-    <Stack m="24">
-      <Stack style={{ maxWidth: 800 }}>
-        <Text size="20" mb="16">
-          {modelName}
-        </Text>
-      </Stack>
+    <Stack m="24" gap="12" style={{ maxWidth: 1000 }}>
+      <Text size="20" mb="16">
+        {modelName}
+      </Text>
     </Stack>
   );
 }
