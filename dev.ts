f8c00d1ee3d251b87b037e0aeb302a2a6273d577
Dante Issaias <dante@issaias.com>
2023-03-27 17:15:58 +0100

    Release

--- dev.ts
+++ dev.ts
@@ -1,6 +1,6 @@
 import { createServer } from './src/server';
 
-createServer({ isProd: true }).then((server) => {
+createServer({}).then((server) => {
   server.listen(5555, () =>
     console.log('> Dash started on http://localhost:5555')
   );
